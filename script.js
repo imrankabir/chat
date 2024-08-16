4910f6a28d7e6679e2b7dd703d5c6c95151f4ced
Imran Kabir <imran.kabir.k@gmail.com>
2024-08-16 11:28:21 +0500

    chat auto commit on 2024-08-16 11:28:21 AM

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const app = 'Chat';
 const VISITS_KEY = 'chat-visits';
 
 const setDimensions = e => {
-  const iframe = document.querySelector("#iframe");
-  iframe.width = window.innerWidth + "px";
-  // iframe.height = window.innerHeight + 'px';
-  // iframe.width = (window.innerWidth - 15) + 'px';
-  iframe.height = window.innerHeight - 5 + "px";
+    const iframe = document.querySelector("#iframe");
+    iframe.width = window.innerWidth + "px";
+    // iframe.height = window.innerHeight + 'px';
+    // iframe.width = (window.innerWidth - 15) + 'px';
+    iframe.height = window.innerHeight - 5 + "px";
 };
+
 setDimensions();
 
 window.onresize = e => setDimensions();
@@ -47,6 +48,7 @@ async function trackVisitor() {
     let visits = JSON.parse(localStorage.getItem(VISITS_KEY)) || [];
     visits.push({ip, time, app});
     localStorage.setItem(VISITS_KEY, JSON.stringify(visits));
+    persistVisits();
 }
 
 async function persistVisits() {
@@ -67,4 +69,3 @@ async function persistVisits() {
 }
 
 trackVisitor();
-persistVisits();
